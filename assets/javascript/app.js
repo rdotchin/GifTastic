a629b44d92be53d8fe4e4c7ef5c928513fa9fc25
rdotchin <richard.dotchin@gmail.com>
2016-11-04 23:24:22 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,10 @@ $(document).ready(function(){
 	//----------------------------------------------------------
 	var topics = ["hockey", "baseball", "football", "lacrosse", "snowboarding", "golf"];
 	var searchWord = [];
-	var gifSelect = [];
 	
 	
-	//ajax
+	
+	//AJAX
 	//----------------------------------------------------------
 	function displayGifs() {
 		var apiKey = "dc6zaTOxFJmzC";
@@ -15,53 +15,41 @@ $(document).ready(function(){
 		
 
 		$.ajax({url: giphyURL, method: 'GET'}).done(function(response) {
-				//clear gifs from webpage
-				$('#gifs').empty();
-				//loop to create gifs on webpage when button clicked
-				for(var i=0; i<10; i++) {
-				$('#gifs').append('<iframe src=' + response.data[i].images.fixed_height.url+ '>');
-				}
-
-				//on click event to change image to gif.  Double click to go back.
-				/*$('iframe').on('click', function() {
-					gifSelect = [];
-					var gifValue = $(this).val();
-					gifSelect.push(gifSelect)
-					console.log(gifSelect);*/
-
-				//maybe another ajax to grab the gif instead of image
-				//double click to go back to image
-
-				})
-			/*})*/
+			//clear gifs from webpage
+			$('#gifs').empty();
+			//loop to create gifs on webpage when button clicked
+			for(var i=0; i<10; i++) {
+			$('#gifs').append('<iframe src=' + response.data[i].images.fixed_height.url+ '>');
+			}
+		})
 	};
 
 	
-	//functions
+	//Functions
 	//--------------------------------------------------------------
 
 	//creates buttons and gives them a class
 	function buildButtons() {
 		//empty buttons from #gifButtons div
-		$('#gifButtons').empty();
+		$('#pageButtons').empty();
 		//repopulate buttons in topics array
 		for(var i = 0; i < topics.length; i++) {
 			var button = $('<button value=' + topics[i] + '>');
 			button.addClass('gifButtons');
 			button.attr('data-name', topics[i]);
 			button.text(topics[i]);
-			$('#gifButtons').append(button);
-			console.log(topics);
+			$('#pageButtons').append(button);
+			
 		}
 	}
 
-	//UI
+	//User Interaction
 	//----------------------------------------------------------------
 	//builds the initial site sports buttons
 	buildButtons();
 
 	//on click function to run buttons word to giphy for search response
-	$('button').on("click", function() {
+	$('.gifButtons').on("click", function() {
 		var value = $(this).val();
 		searchWord = [];
 		searchWord.push(value);
@@ -71,23 +59,43 @@ $(document).ready(function(){
 
 	//on click to create new sports topic button
 	$('#addGif').on('click', function() {
-
+		//gets the value of #gif-input and trims the word, pushes to topics
 		var sport = $('#gif-input').val().trim();
 		topics.push(sport);
 		buildButtons();
+		console.log(topics);
 		return false;
 		
 		
 	})
 
 
+	
 
 
-//for ratings	
-				/*$('#gifs').append("rating: " + response.data[i].rating );*/
-				//for images
-				/*$('#gifs').append('<iframe src=' + response.data[i].images.fixed_height_still.url + '>');*/
-				//for gifs
+
+
+
+
+
+
+	//NOTES
+	//-----------------------------------------------------------------
+		//on click event to change image to gif.  Double click to go back.
+				/*$('iframe').on('click', function() {
+					gifSelect = [];
+					var gifValue = $(this).val();
+					gifSelect.push(gifSelect)
+					console.log(gifSelect);*/
+
+				//maybe another ajax to grab the gif instead of image
+				//double click to go back to image
+
+			//for ratings	
+			/*$('#gifs').append("rating: " + response.data[i].rating );*/
+			//for images
+			/*$('#gifs').append('<iframe src=' + response.data[i].images.fixed_height_still.url + '>');*/
+			//for gifs
 
 
 
