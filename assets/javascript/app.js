0973ef7b55e22c940fe09850ef1a71941005aeed
rdotchin <richard.dotchin@gmail.com>
2016-11-08 02:37:29 -0500

    gifs working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,20 +21,36 @@ $(document).ready(function(){
 			console.log(giphyURL);
 			//loop to create gifs on webpage when button clicked
 			for(var i=0; i<10; i++) {
-				/*$('#gifs').append("rating: " + response.data[i].rating )*/
-				var gifImage = $('<img src=' + response.data[i].images.fixed_height_still.url + '>');
-				var gifGif = $('<iframe src=' + response.data[i].images.fixed_height.url + '>')
-				gifImage.attr('data-value', response.data[i].images.fixed_height.url);
+				var image = response.data[i].images.fixed_height_still.url;
+				var animated = response.data[i].images.fixed_height.url;
+				var rating = response.data[i].rating;
+				var gifImage = $('<img src=' + image + '>');
+				//add class to gifs
+				gifImage.addClass('gifs');
+				gifImage.attr('id', 'gif' + [i]);
+				gifImage.attr('data-still', image);
+				gifImage.attr('data-animate', animated);
+				gifImage.attr('data-state', 'still');
+				$('#gifs').append('<p>Rating: ' + rating);
 				$('#gifs').append(gifImage);
-				$('#gifs').append(gifGif);
-				$('iframe').hide();
+				
+				
 			}
 
-			$('#gifs').on("click", "img", function() {
-				$('img').hide();
-				$('iframe').show();
-			});	
-		});
+		$('.gifs').on('click', function() {
+			var state = $(this).attr('data-state');
+
+			if ( state == 'still'){
+				$(this).attr('src', $(this).data('animate'));
+				$(this).attr('data-state', 'animate');
+			}
+			else {
+				$(this).attr('src', $(this).data('still'));
+				$(this).attr('data-state', 'still');
+			}
+		})
+
+		})
 			
 	};
 
@@ -49,13 +65,13 @@ $(document).ready(function(){
 		//repopulate buttons in topics array
 		for(var i = 0; i < topics.length; i++) {
 			var button = $('<button value=' + topics[i] + '>');
-			button.addClass('gifButtons');
+			button.addClass('sportsButtons');
 			button.attr('data-name', topics[i]);
 			button.text(topics[i]);
 			$('#pageButtons').append(button);
 			
 		}
-			$('.gifButtons').on("click", function() {
+			$('.sportsButtons').on("click", function() {
 		var value = $(this).val();
 		searchWord = [];
 		searchWord.push(value);
@@ -72,12 +88,22 @@ $(document).ready(function(){
 	//on click to create new sports topic button
 	$('#addGif').on('click', function() {
 		//gets the value of #gif-input and trims the word, pushes to topics
+		for(var i=0; i<topics.length; i++) {
 		var sport = $('#gif-input').val().trim();
+		console.log(sport);
+		//want to stop duplicate buttons being created
+		if(topics[i]==sport){
+			alert("already a button");
+			$('#addGif').val('');
+		}
+		else if(topics[i] !== sport){
 		topics.push(sport);
 		buildButtons();
 		console.log(topics);
+		$('#gif-input').val('');
 		return false;
+		}
 		
-		
-	})
+	}
+})
 });
